989ea06a8f69f2052cde85241cdb874b4fa2ab61
999salem <123068517+999salem@users.noreply.github.com>
2026-09-09 13:32:45 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='doombot-v18-6-persistent-bottom-nav';
+const CACHE='doombot-v18-6-1-hard-pinned-nav';
 const CORE=[
   './','./index.html','./intel.html','./leaks.html','./theories.html',
   './analyst.html','./plot.html','./credits.html','./about.html','./settings.html','./offline.html',
